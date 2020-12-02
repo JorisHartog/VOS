04e238b41f9a2e8f57c934abef73392e2eff981a
Joris Hartog <jhartog@hostnet.nl>
2020-12-02 21:12:04 +0100

    Improve printf function

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,18 +1,53 @@
 #include "types.h"
 #include "gdt.h"
 
-void printf(char* str) {
-  uint16_t* VideoMemory = (uint16_t*)0xb8000;
+void printf(const char* str) {
+  static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+  static uint8_t x=0, y=0;
 
   for (int i = 0; str[i] != '\0'; i++) {
     // Copy each character to the video memory, but retain the high byte which
     // contains color info.
-    VideoMemory[i] = (VideoMemory[i] & 0xff00) | str[i];
+    switch(str[i]) {
+      case '\n':
+        x = 0;
+        y++;
+        break;
+      default:
+        VideoMemory[y*80 + x] = (VideoMemory[i] & 0xff00) | str[i];
+        x++;
+    }
+  }
+
+  if (x >= 80) {
+    x = 0;
+    y++;
+  }
+
+  if (y >= 25) {
+    for (y = 1; y < 25; y++)
+      for (x = 0; x < 80; x++)
+        VideoMemory[(y-1)*80 + x] = VideoMemory[y*80 + x];
+
+    x = 0;
+    y = 24;
   }
 }
 
 extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber) {
-  printf("Hi there! :D");
+  printf("                     .-'''-.       \n");
+  printf("                    '   _    \\     \n");
+  printf(" .----.     .----./   /` '.   \\    \n");
+  printf("  \\    \\   /    /.   |     \\  '    \n");
+  printf("   '   '. /'   / |   '      |  '   \n");
+  printf("   |    |'    /  \\    \\     / /    \n");
+  printf("   |    ||    |   `.   ` ..' / _   \n");
+  printf("   '.   `'   .'      '-...-'`.' |  \n");
+  printf("    \\        /              .   | /\n");
+  printf("     \\      /             .'.'| |//\n");
+  printf("      '----'            .'.'.-'  / \n");
+  printf("                        .'   \\_.'  \n");
+  printf("Written by Joris Hartog\n");
 
   GlobalDescriptorTable gdt;
 
